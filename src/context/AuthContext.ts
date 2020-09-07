cf93064392ff321859f8db772222475b97df87fb
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2020-09-07 15:00:20 -0300

    start authcontext

---
+++ src/context/AuthContext.ts
@@ -0,0 +1,8 @@
+import { createContext } from 'react';
+
+interface AuthContext {
+  name: string;
+}
+
+const authContext = createContext<AuthContext>({} as AuthContext);
+export default authContext;
