9e3018158081746e200f1c53a8753fb1051b6259
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2020-09-06 22:46:28 -0300

    signin

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Container, Content, Background } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { FiLogIn } from 'react-icons/fi';
+// import backgroundImg from '../../assets/sign-in-background.png';
 
 // import { Container } from './styles';
 
-const SignIn: React.FC = () => {
-  return <div />;
-};
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+      <form>
+        <h1>Entre no GoBarber</h1>
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <button type="submit">Entrar</button>
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+      <a href="">
+        <FiLogIn />
+        Criar Conta
+      </a>
+    </Content>
+    <Background></Background>
+  </Container>
+);
 
 export default SignIn;
